2411f422a2b838f62d88adf7792471d6af928bfa
Todd Grabowsky <todd.grabowsky@gmail.comgit credential-osxkeychain>
2016-08-22 23:56:50 -0400

    updated background animation

--- assets/js/particle-network.js
+++ assets/js/particle-network.js
@@ -90,7 +90,7 @@
     this.ctx = this.canvas.getContext('2d');
     this.canvas.width=window.innerWidth;
     this.canvas.height=window.innerHeight;
-    // this.canvas.style.display='block';
+    this.canvas.style.display='block';
     // //this.setStyles(this.canvasDiv, { 'position': 'relative' });
     this.setStyles(this.canvas, {
       'z-index': '20',
